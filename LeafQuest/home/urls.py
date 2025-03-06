f764d465ecc7c6c5a8f63298ebf2d803582d36b7
Zach Middleton <zach.middleton12@yahoo.com>
2025-03-06 23:23:07 +0000

    user registration set up; added Profile model

--- LeafQuest/home/urls.py
+++ LeafQuest/home/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import home_view, profile_view
+from django.urls import include, path
+from .views import *
 
 urlpatterns = [
     path('', home_view, name="home"),
-    path('profile', profile_view, name="profile")
+    path('profile', profile_view, name="profile"),
+
+    # account management
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', registerPage, name='register_page')
 ]
--- LeafQuest/home/views.py
+++ LeafQuest/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 def home_view(request):
@@ -6,3 +8,21 @@ def home_view(request):
 
 def profile_view(request):
     return render(request, 'profile/index.html')
+
+# user registration page
+def registerPage(request):
+      form = CreateUserForm()
+
+      if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                  user = form.save()
+                  username = form.cleaned_data.get('username')
+                  profile = Profile.objects.create(user=user,)
+                  profile.save()
+
+                  messages.success(request, 'Account was created for ' + username)
+                  return redirect('login')
+       
+      context = {'form': form}
+      return render(request, 'registration/register.html', context)
