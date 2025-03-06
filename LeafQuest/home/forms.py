2622222658dea43f19e251534e02ef4047d12329
Zach Middleton <zach.middleton12@yahoo.com>
2025-03-06 23:23:07 +0000

    user registration set up; added Profile model

---
+++ LeafQuest/home/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        exclude = ['user']
---
+++ LeafQuest/home/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .forms import *
+from django.contrib import messages
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'home/index.html')
+
+def profile_view(request):
+    return render(request, 'profile/index.html')
+
+def capture_view(request):
+    return render(request, 'capture/index.html')
+
+def plantdex_view(request):
+    return render(request, 'plantdex/index.html')
+
+def badges_view(request):
+    return render(request, 'badges/index.html')
+
+def map_view(request):
+    return render(request, 'map/index.html')
+
+def social_view(request):
+    return render(request, 'social/index.html')
+
+def settings_view(request):
+    return render(request, 'settings/index.html')
+
+def logout_view(request):
+    return render(request, 'logout/index.html')
+
+def about_view(request):
+    return render(request, 'about/index.html')
+    
+# user registration page
+def registerPage(request):
+      form = CreateUserForm()
+
+      if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                  user = form.save()
+                  username = form.cleaned_data.get('username')
+                  profile = Profile.objects.create(user=user,)
+                  profile.save()
+
+                  messages.success(request, 'Account was created for ' + username)
+                  return redirect('login')
+       
+      context = {'form': form}
+      return render(request, 'registration/register.html', context)
