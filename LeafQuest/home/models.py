2b87af208a7a36eb6926e7242d51071b47aeff92
Zach Middleton <zach.middleton12@yahoo.com>
2025-03-08 01:11:39 +0000

    added profile pictures; set up some dummy templates/views for testing

--- LeafQuest/home/models.py
+++ LeafQuest/home/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    aboutMe = models.TextField(blank=True)
+    aboutMe = models.TextField(max_length=250, blank=True)
+    pfp = models.ImageField(upload_to='profile/', blank=True)
+    # to be added - friends
 
     def __str__(self):
         return self.name + '(' + self.user.username + ')'
