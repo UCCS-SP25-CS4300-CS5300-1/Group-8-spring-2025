4fc396913dd0d390ab3b2fb4918cb1fb2707e392
Zach Middleton <zach.middleton12@yahoo.com>
2025-03-08 01:11:39 +0000

    added profile pictures; set up some dummy templates/views for testing

--- LeafQuest/LeafQuest/settings.py
+++ LeafQuest/LeafQuest/settings.py
@@ -131,6 +131,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/images/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
