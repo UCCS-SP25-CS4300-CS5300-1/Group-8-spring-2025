ad2b06c911a8eb55dd024c680260c388d322b027
Zach Middleton <zach.middleton12@yahoo.com>
2025-03-12 03:45:33 +0000

    created models for friend lists and friend requests

--- LeafQuest/home/models.py
+++ LeafQuest/home/models.py
@@ -1,14 +1,63 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# user profile/account
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     aboutMe = models.TextField(max_length=250, blank=True)
     pfp = models.ImageField(upload_to='profile/', blank=True)
-    # to be added - friends
 
+    # more readable name for model
     def __str__(self):
         return self.name + '(' + self.user.username + ')'
 
+# friend list for a given user
+class FriendList(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(Profile, blank=True, related_name='friends')
+ 
+    # add a friend
+    def add_friend(self, friend):
+        # add friend if friend isn't already added
+        if not friend in self.friends.all():
+            self.friends.add(friend)
 
+    # remove a friend
+    def remove_friend(self, friend):
+        # remove friend if friend is added
+        if friend in self.friends.all():
+            self.friends.remove(friend)
+
+# friend request from one user to another
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+
+    pending = models.BooleanField(blank=True, default=True)
+
+    # more readable name for model
+    def __str__(self):
+        return self.sender.name + ' -> ' + self.reveiver.name
+    
+    # accept a friend request
+    def accept(self):
+        sender_friends = FriendList.objects.get(profile=self.sender)
+        receiver_friends = FriendList.objects.get(profile=self.receiver)
+
+        # check that both friend lists exist
+        if sender_friends and receiver_friends:
+            sender_friends.add_friend(self.receiver)
+            receiver_friends.add_friend(self.sender)
+
+    # decline friend request (receiver side)
+    def decline(self):
+        # set pending field to false to denote that request is declined
+        self.pending = False
+        self.save()
+
+    # cancel friend request (sender side)
+    def cancel(self):
+        # set pending field to false to denote that request is cancelled
+        self.pending = False
+        self.save()
