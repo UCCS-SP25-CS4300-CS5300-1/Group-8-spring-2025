a442a4b573752439c961e726f6ab116c42d0fdf3
Aclamendo <davidjones@ycre.net>
2025-03-13 15:11:31 -0600

    Static File Delivery Test

--- LeafQuest/home/tests/ci_test.py
+++ LeafQuest/home/tests/ci_test.py
@@ -1,7 +1,25 @@
 
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+
 
 # Create your tests here.
 class CITest(TestCase):
     def test_sanity(self):
         self.assertTrue(True)
+
+
+class CDBuildTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # Ensure the test server loads static files
+        #call_command('collectstatic', verbosity=0, interactive=False)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_css_delivery(self):
+        response = self.client.get('/static/css/style.css')
+        self.assertEqual(response.status_code, 200)
