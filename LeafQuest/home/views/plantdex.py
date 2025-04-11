d437bcb6cdab89567d92592eb89908e1798cd321
wycre <djones@wycre.net>
2025-04-11 15:00:38 -0600

    Capture details page

--- LeafQuest/home/views/plantdex.py
+++ LeafQuest/home/views/plantdex.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from ..models import Profile
 from ..models.captured_image_model import CapturedImage
 
 
@@ -12,4 +13,16 @@ def plantdex_view(request):
     context['captures'] = captures
 
     return render(request, 'plantdex/index.html', context)
+
+@login_required
+def plantdex_detail_view(request, pk):
+    context = {}
+
+    capture = CapturedImage.objects.get(pk=pk)
+    context['capture'] = capture
+
+    context['user'] = capture.user
+    context['profile'] = Profile.objects.get(user=capture.user)
+
+    return render(request, 'plantdex/details.html', context)
     
