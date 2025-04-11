f6fbd0a78bce716fd1a29192fc8b89c04f2a7388
wycre <djones@wycre.net>
2025-04-11 12:45:33 -0600

    fix page inclusion

--- LeafQuest/home/views/plantdex.py
+++ LeafQuest/home/views/plantdex.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from ..models.captured_image_model import CapturedImage
+
 
 @login_required
 def plantdex_view(request):
-    return render(request, 'plantdex/index.html')
+    context = {}
+
+    captures = CapturedImage.objects.filter(user=request.user)
+    context['captures'] = captures
+
+    return render(request, 'plantdex/index.html', context)
     
