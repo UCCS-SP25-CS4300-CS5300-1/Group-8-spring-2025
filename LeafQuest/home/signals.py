1448f24e8c95c1a09070367b680c4686108c2e7f
keegssmith <smith.keegan18@gmail.com>
2025-04-22 19:43:51 +0000

    fix signals

--- LeafQuest/home/signals.py
+++ LeafQuest/home/signals.py
@@ -12,27 +12,6 @@ from django.db.models.signals import post_migrate
 from django.apps import apps
 from django.dispatch import receiver
 
-
-@receiver(post_save, sender=CapturedImage)
-def update_leaderboard_entry(sender, instance, created, **kwargs):
-    user = instance.user
-    this_entry = Leaderboard.objects.get_or_create(user=user)[0]
-    if not this_entry.profile:
-        this_entry.profile = Profile.objects.get(user=user)
-
-    # Update the capture number
-    this_entry.num_captures = this_entry.num_captures + 1
-    this_entry.save()
-
-
-@receiver(post_save, sender=Leaderboard)
-def update_leaderboard(sender, instance, created, **kwargs):
-    all_entries = Leaderboard.objects.all().order_by('-num_captures')
-
-    for i in range(len(all_entries)):
-        entry = all_entries[i]
-        Leaderboard.objects.filter(pk=entry.pk).update(rank=i+1)
-
 @receiver(post_save, sender=CapturedImage)
 def update_leaderboard_entry(sender, instance, created, **kwargs):
     user = instance.user
@@ -60,6 +39,14 @@ def update_leaderboard_entry(sender, instance, created, **kwargs):
                     badge.users.add(user)
             except Badge.DoesNotExist:
                 pass 
+
+@receiver(post_save, sender=Leaderboard)
+def update_leaderboard(sender, instance, created, **kwargs):
+    all_entries = Leaderboard.objects.all().order_by('-num_captures')
+
+    for i in range(len(all_entries)):
+        entry = all_entries[i]
+        Leaderboard.objects.filter(pk=entry.pk).update(rank=i+1)
 
 @receiver(post_migrate)
 def create_default_badges(sender, **kwargs):
