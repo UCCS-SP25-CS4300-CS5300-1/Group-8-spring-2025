eaecab7ea2bce18eb3c71cb2ee3133073b1b6b8e
wycre <djones@wycre.net>
2025-04-29 14:38:09 -0600

    Finish preferences form - Also update django messages style

--- LeafQuest/home/views/settings.py
+++ LeafQuest/home/views/settings.py
@@ -3,9 +3,11 @@ Views for user settings
 """
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from ..models import ClientPreferences
 from ..forms.preferences_forms import PreferencesForm
+
 
 @login_required
 def settings_view(request):
@@ -16,3 +18,20 @@ def settings_view(request):
         form = PreferencesForm(instance=preferences)
         context['form'] = form
         return render(request, 'settings/index.html', context)
+
+    if request.method == "POST":
+        preferences = ClientPreferences.objects.filter(user=request.user)[0]
+        form = PreferencesForm(request.POST)
+        if form.is_valid():
+            use_dark_mode = form.cleaned_data['use_dark_mode']
+            preferences.use_dark_mode = use_dark_mode
+            preferences.save()
+
+            context['form'] = form
+            messages.success(request, 'Settings Saved')
+
+            return render(request, 'settings/index.html', context)
+        else:
+            context['form'] = form
+            messages.error(request, 'Error saving settings\n' + form.errors)
+            return render(request, 'settings/index.html', context)
