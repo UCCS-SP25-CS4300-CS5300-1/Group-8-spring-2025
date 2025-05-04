39f02c2011447263c876def8aea5ae7764b8894c
Zach Middleton <zach.middleton12@yahoo.com>
2025-05-04 18:25:39 +0000

    added badges to home page

--- LeafQuest/home/views/home.py
+++ LeafQuest/home/views/home.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from ..models.captures import CapturedImage
+from ..models.badge_model import Badge
 
 
 @login_required
@@ -14,7 +15,12 @@ def home_view(request):
     # Get captured images
     captures = CapturedImage.objects.filter(user=request.user).order_by('-uploaded_at')
     all_captures = CapturedImage.objects.all().order_by('-uploaded_at')
+
+    # badges
+    badges = Badge.objects.all()
+
     context['captures'] = captures
     context['all_captures'] = all_captures
+    context['badges'] = badges
 
     return render(request, 'home/index.html', context)
