2ec558d7d58ba342ad59919c69bf59ed14c54c7b
keegssmith <smith.keegan18@gmail.com>
2025-05-05 05:06:14 +0000

    lint fix

--- LeafQuest/home/views/home.py
+++ LeafQuest/home/views/home.py
@@ -14,10 +14,10 @@ def home_view(request):
 
     captures = CapturedImage.objects.filter(user=request.user).order_by('-uploaded_at')
     all_captures = CapturedImage.objects.all().order_by('-uploaded_at')[:60]
-    unlocked_badges = Badge.objects.filter(users=request.user).order_by('name') 
+    unlocked_badges = Badge.objects.filter(users=request.user).order_by('name')
 
     context['captures'] = captures
     context['all_captures'] = all_captures
-    context['unlocked_badges'] = unlocked_badges 
+    context['unlocked_badges'] = unlocked_badges
 
     return render(request, 'home/index.html', context)
