b6a716f8b091bf0792d0bc17e6e0611735d9d6a6
keegssmith <smith.keegan18@gmail.com>
2025-05-05 04:59:13 +0000

    home page UI changes

--- LeafQuest/home/views/home.py
+++ LeafQuest/home/views/home.py
@@ -5,16 +5,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from ..models.captured_image_model import CapturedImage
+from ..models.badge_model import Badge
 
 
 @login_required
 def home_view(request):
     context = {}
 
-    # Get captured images
     captures = CapturedImage.objects.filter(user=request.user).order_by('-uploaded_at')
-    all_captures = CapturedImage.objects.all().order_by('-uploaded_at')
+    all_captures = CapturedImage.objects.all().order_by('-uploaded_at')[:60]
+    unlocked_badges = Badge.objects.filter(users=request.user).order_by('name') 
+
     context['captures'] = captures
     context['all_captures'] = all_captures
+    context['unlocked_badges'] = unlocked_badges 
 
     return render(request, 'home/index.html', context)
